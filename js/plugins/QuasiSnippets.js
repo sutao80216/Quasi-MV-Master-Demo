23c462e1f29f150631d69168431adfd77d9720c1
Quasi <milio11@yahoo.com>
2016-04-16 21:57:03 -0700

    Update
    New snippet version

--- js/plugins/QuasiSnippets.js
+++ js/plugins/QuasiSnippets.js
@@ -1,11 +1,11 @@
 //============================================================================
 // Quasi Snippets
-// Version: 1.0.0
-// Last Update: October 23, 2015
+// Version: 1.02
+// Last Update: April 1, 2016
 //============================================================================
 
 var Imported = Imported || {};
-Imported.Quasi_Snippets = 1.0;
+Imported.Quasi_Snippets = 1.02;
 
 //=============================================================================
  /*:
@@ -17,10 +17,25 @@ Imported.Quasi_Snippets = 1.0;
  * Set to true or false
  * @default true
  *
- * @param Demo Text
- * @desc Turns switch 1 on or off on new game with Quick Test
+ * @param Default Enabled Switches
+ * @desc Turns on a list of switches on by default
+ * Each switch should be seperated by a comma.
+ * @default 1
+ *
+ * @param Debug
+ * @desc Turns on Console by Default
  * Set to true or false
  * @default true
+ *
+ * @param Screen Width
+ * @desc Set the default Screen Width
+ * Default: 1104
+ * @default 1104
+ *
+ * @param Screen Height
+ * @desc Set the default Screen Width
+ * Default: 624
+ * @default 624
   */
  //=============================================================================
 
@@ -28,7 +43,19 @@ Imported.Quasi_Snippets = 1.0;
   var Quasi = {};
   Quasi.param      = PluginManager.parameters('QuasiSnippets');
   Quasi.quickStart = (Quasi.param['Quick Test'].toLowerCase() == 'true');
-  Quasi.demoText   = (Quasi.param['Demo Text'].toLowerCase() == 'true');
+  Quasi.debug      = (Quasi.param['Debug'].toLowerCase() == 'true');
+  Quasi.switches   = Quasi.param['Default Enabled Switches'].split(",").map(function(n) { return Number(n) || 0 });
+
+  SceneManager._screenWidth       = Number(Quasi.param['Screen Width']);
+  SceneManager._screenHeight      = Number(Quasi.param['Screen Height']);
+  SceneManager._boxWidth          = Number(Quasi.param['Screen Width']);
+  SceneManager._boxHeight         = Number(Quasi.param['Screen Height']);
+
+  window.resizeBy(SceneManager._screenWidth - window.innerWidth, SceneManager._screenHeight - window.innerHeight);
+
+  if (Quasi.debug) {
+    require('nw.gui').Window.get().showDevTools();
+  }
 
   var Alias_Scene_Boot_start = Scene_Boot.prototype.start;
   Scene_Boot.prototype.start = function() {
@@ -40,10 +67,25 @@ Imported.Quasi_Snippets = 1.0;
       this.checkPlayerLocation();
       DataManager.setupNewGame();
       SceneManager.goto(Scene_Map);
-      $gameSwitches.setValue(1, Quasi.demoText);
+      for (var i = 0; i < Quasi.switches.length; i++) {
+        $gameSwitches.setValue(Quasi.switches[i], true);
+      }
       this.updateDocumentTitle();
     } else {
       Alias_Scene_Boot_start.call(this);
     }
+  };
+
+  ImageManager.isReady = function() {
+    for (var key in this._cache) {
+      var bitmap = this._cache[key];
+      if (bitmap.isError()) {
+        this._cache[key] = new Bitmap();
+      }
+      if (!bitmap.isReady()) {
+        return false;
+      }
+    }
+    return true;
   };
 })();
